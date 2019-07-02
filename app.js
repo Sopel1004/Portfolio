eb76cd603fa03e02d00c13d8cba5029dcfa58805
Sopel1004 <47826072+Sopel1004@users.noreply.github.com>
2019-07-03 01:08:28 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -13,15 +13,6 @@ const showMenu = () => {
     document.querySelector('body').classList.toggle('lockScroll');
 }
 
-const changeHeaderBgColor = () => {
-    if (window.pageYOffset > 0) {
-        header.style.opacity = "0";
-    }
-    else {
-        header.style.opacity = "0";
-    }
-}
-
 const scrollToFirstElement = () => {
     window.scrollTo({
         left: 0,
@@ -39,42 +30,21 @@ const jumpToAnchor = e => {
 }
 
 const changeColor = () => {
-    if (colorChanger.checked) {
+    colorChanger.classList.toggle('active');
+    if (colorChanger.classList.contains('active')) {
         document.querySelector('body').classList.toggle('darkMotif');
         menu.classList.toggle('darkMotif');
-        header.classList.toggle('darkMotif');
     }
     else {
         document.querySelector('body').classList.toggle('darkMotif');
         menu.classList.toggle('darkMotif');
-        header.classList.toggle('darkMotif');
     }
 }
-
-//do pomyślenia
-/*const lockView = () => {
-    console.log(`Scroll:${window.pageYOffset}`);
-    let currentSection;
-    for (let i = 0; i < sections.length; i++) {
-        if (sections[i].offsetTop - 200 <= window.pageYOffset && window.pageYOffset < (sections[i].offsetTop + sections[i].offsetHeight))
-            currentSection = sections[i];
-    }
-    if (currentSection.offsetTop - 100 <= window.pageYOffset && window.pageYOffset < currentSection.offsetTop)
-        setTimeout(window.scrollTo({
-            left: 0,
-            top: currentSection.offsetTop - 40,
-            behavior: "smooth",
-        }), 2000);
-
-
-}*/
 
 menuBtn.addEventListener('click', showMenu);
 menuElement.forEach(el => {
     el.addEventListener('click', showMenu);
     el.addEventListener('click', jumpToAnchor);
 });
-window.addEventListener('scroll', changeHeaderBgColor);
 arrowDown.addEventListener('click', scrollToFirstElement);
 colorChanger.addEventListener('click', changeColor);
-//window.addEventListener('scroll', lockView);
